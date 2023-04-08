e1a778d45abbdaf49701bdafab2f9d1c52462fa5
crazyrussian123456 <122416613+crazyrussian123456@users.noreply.github.com>
2023-04-08 15:24:14 -0600

    Attempting to upload agario step two

---
+++ Agario/TowardAgarioStepTwo copy/Person.cs
@@ -0,0 +1,10 @@
+﻿public class Person
+{
+    public Person()
+    {
+
+    }
+    public float GPA = 4;
+    private string Name { get; set; } = "Jim";
+    int ID { get; set; } = 1;
+}
---
+++ Agario/TowardAgarioStepTwo copy/TowardAgarioStepTwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+
+namespace TowardAgarioStepTwo
+{
+	public class TowardAgarioStepTwo
+	{
+        //private Person person = new Person();
+
+        static void Main(String[] args)
+		{
+            var person = new Person();
+            
+            string message = JsonSerializer.Serialize(person);
+            Console.WriteLine(message);
+        }
+    }
+}
+
