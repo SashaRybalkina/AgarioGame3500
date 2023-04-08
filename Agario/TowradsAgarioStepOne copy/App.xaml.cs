d4f4426a167e43435ffd68b062317c1410d43793
crazyrussian123456 <122416613+crazyrussian123456@users.noreply.github.com>
2023-04-08 14:28:07 -0600

    Attempting to commit agario step 1 files

---
+++ Agario/TowradsAgarioStepOne copy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TowradsAgarioStepOne;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Agario/TowradsAgarioStepOne copy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TowradsAgarioStepOne;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
