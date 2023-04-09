7b4c0e5ebd9659ac7ce01ba33d6892527429108b
sasharybalkina <sashaawesomeness@gmail.com>
2023-04-09 15:23:44 -0600

    made progress on step 3 and finnished step 2

--- Agario/TowardAgarioStepThree/Program.cs
+++ Agario/TowardAgarioStepThree/Program.cs
@@ -1,20 +1,22 @@
-﻿using Communications;
+﻿using System;
+using System.Text.Json;
+using Communications;
 using FileLogger;
 using Microsoft.Extensions.Logging;
 
-public class Main
+public class Program
 {
     private static Networking network;
     private static CustomFileLogger _logger = new CustomFileLogger("StepThree");
 
-    public static void main(string[] agrs)
+    public static void Main(String[] args)
     {
-        network = new Networking(_logger, onConnect, onDisconnect, onMessage, '\n');
+        network = new Networking(_logger, onMessage, onDisconnect, onConnect, '\n');
         network.Connect("localhost", 11000);
         network.AwaitMessagesAsync();
         Console.ReadLine();
     }
-    private static async void onConnect(Networking net, string name)
+    private static async void onConnect(Networking net)
     {
 
     }
@@ -24,8 +26,15 @@ public class Main
 
     }
 
-    private static async void onMessage(Networking net)
+    private static async void onMessage(Networking net, string message)
     {
-        Console.WriteLine(network);
+        Console.WriteLine(message);
+        string foodString = "";
+        if (message.StartsWith("CMD_Food"))
+        {
+            Console.WriteLine(message);
+            foodString = message.Remove(0, 8);
+            String[] ?food = JsonSerializer.Deserialize<String[]>(foodString);
+        }
     }
 }
